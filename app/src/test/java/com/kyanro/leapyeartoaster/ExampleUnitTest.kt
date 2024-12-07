cf74e77e0dd10a8470cdef344b8c683e55ad101d
kyanro <kyanro@users.noreply.github.com>
2024-12-08 04:29:16 +0900

    テストメンテ
    不要テスト削除
    Junit5形式に変更

--- app/src/test/java/com/kyanro/leapyeartoaster/ExampleUnitTest.kt
+++ app/src/test/java/com/kyanro/leapyeartoaster/ExampleUnitTest.kt
@@ -1,17 +1,26 @@
 package com.kyanro.leapyeartoaster
 
-import org.junit.Test
-
-import org.junit.Assert.*
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import org.junit.jupiter.api.extension.ExtendWith
+import tech.apter.junit.jupiter.robolectric.RobolectricExtension
+import kotlin.test.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+@ExtendWith(RobolectricExtension::class)
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assert(4 == 2 + 2)
+    }
+
+    @Test
+    fun パッケージ名が取得できる() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        assert("com.kyanro.leapyeartoaster" == context.packageName)
     }
 }
